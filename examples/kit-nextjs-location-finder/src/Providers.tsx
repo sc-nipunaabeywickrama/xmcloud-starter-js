2fd6cba814116bc35bc4f770cee64a0e743b67d9
Nipuna Abeywickrama <nipu@sitecore.net>
2026-07-24 10:33:03 +0530

    Add atom components and schemas for button, heading, image, number, and paragraph in kit-nextjs-location-finder example

--- examples/kit-nextjs-location-finder/src/Providers.tsx
+++ examples/kit-nextjs-location-finder/src/Providers.tsx
@@ -7,6 +7,7 @@ import components from '.sitecore/component-map.client';
 import { ThemeProvider } from '@/components/theme-provider/theme-provider.dev';
 import { VideoProvider } from './contexts/VideoContext';
 import { loadFramerFeatures } from '@/lib/framer-features';
+import { catalog, registry } from '@/atoms';
 
 export default function Providers({
   children,
@@ -21,6 +22,7 @@ export default function Providers({
       componentMap={components}
       page={page}
       loadImportMap={() => import('.sitecore/import-map.client')}
+      atomsConfig={{catalog, registry}}
     >
       <LazyMotion features={loadFramerFeatures} strict>
         <VideoProvider>
